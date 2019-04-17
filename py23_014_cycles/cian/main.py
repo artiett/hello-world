3cb6ac70e2f88f8a3a87e86d037dc1b871fe7ab9
Artyom <artiett@users.noreply.github.com>
2019-04-17 15:13:22 +0300

    homework revised

--- py23_014_cycles/cian/main.py
+++ py23_014_cycles/cian/main.py
@@ -1,29 +1,43 @@
 import csv
-
+from pprint import pprint
 flats_list = list()
-
 with open('output.csv', newline='') as csvfile:
-	flats_csv = csv.reader(csvfile, delimiter=';')
-	flats_list = list(flats_csv)
+    flats_csv = csv.reader(csvfile, delimiter=';')
+    flats_list = list(flats_csv)
+header = flats_list.pop(0)
 
-
-#TODO 1:
+# цикл с порядковыми номерами новостроек
 count = 0
 print('Новостройки:')
 for i, flat in enumerate(flats_list):
-  if "новостройка" in flat[2]:
-    print("Квартира №{} с ID {}, стоит {} руб.".format(i+1, flat[0], flat[11]))
-    count += 1
+    if "новостройка" in flat[2]:
+        print("Квартира №{} с ID {}"
+              .format(i+1, flat[0]))
+        count += 1
+
+# подсчет количества новостроек
 print('Итого: {} новостройки\n\n'.format(count))
 
 
-#TODO 2:
-flat_info = {"id": flat[0], "rooms": flat[1], "type": flat[2], "price": flat[11]}
+# описание квартиры в виде словаря
+flat_info = {}
+for flat in flats_list:
+    flat_info = {"id": flat[0], "rooms": flat[1],
+                 "type": flat[2], "price": flat[11]}
+    print('В квартире с ID {} {} комнат. {}, стоит {}₽'
+          .format(flat_info['id'], flat_info['rooms'],
+                  flat_info['type'].capitalize(), flat_info['price']))
 
+# квартиры, собранные по метро
 subway_dict = {}
 for flat in flats_list:
-  subway = flat[3].replace("м.", "")
-  if subway not in subway_dict.keys():
-    subway_dict[subway] = list()
-  subway_dict[subway].append(flat_info)
-print(subway_dict)
+    subway = flat[3].replace("м.", "")
+    if subway not in subway_dict.keys():
+        subway_dict[subway] = list()
+    subway_dict[subway].append(flat[0:3])
+pprint(subway_dict)
+
+# подсчет квартир у каждого метро
+for subway in subway_dict:
+    print('У метро {} {} квартир'
+          .format(subway, len(subway_dict[subway])))
